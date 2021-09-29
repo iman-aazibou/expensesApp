7c5724c7a2807be19584eebc7e0c3dba351e8774
iman-aazibou <imanaazibou@hotmail.com>
2021-09-29 21:28:02 +0200

    function to show expenses

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Expenses from './components/Expenses/Expenses';
+
+
 
 function App() {
+  const data = [
+    { id: 1, title: "new desk", amount: 300, date: new Date(2021, 5, 12) },
+    { id: 2, title: "new table", amount: 500, date: new Date(2021, 6, 12) },
+    { id: 4, title: "new chair", amount: 200, date: new Date(2021, 8, 12) },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Let's get started</p>
+      <Expenses expenses={data}/>
+   
+      
     </div>
   );
 }
