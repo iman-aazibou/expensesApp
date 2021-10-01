f4bb8ddf0d5a9fc35bfe98d11e4b56a78a508cad
iman-aazibou <imanaazibou@hotmail.com>
2021-10-01 18:31:43 +0200

    lifting the state up to NewExpense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const Expenseform = () => {
+const Expenseform = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -27,7 +27,7 @@ const Expenseform = () => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
