1c05b97ffd0ec0795ccc43e5b56f41e655670b34
iman-aazibou <imanaazibou@hotmail.com>
2021-10-02 14:57:49 +0200

    mapping through each expense item component to be added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,34 +4,24 @@ import Card from "./../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 function Expenses(props) {
-  const [expenseYear, setExpenseYear] = useState('2021');
+  const [expenseYear, setExpenseYear] = useState("2021");
 
   const setSelectedYear = (yearselected) => {
-    console.log(yearselected)
+    console.log(yearselected);
     setExpenseYear(yearselected);
   };
 
   return (
-    
-      <Card className="expenses">
+    <Card className="expenses">
       <ExpensesFilter selected={expenseYear} onYearSelect={setSelectedYear} />
+      {props.expenses.map((expense) => (
         <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpenseItem>
-      </Card>
-  
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
   );
 }
 
