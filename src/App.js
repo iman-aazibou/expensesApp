1b5a2177d9024963b643a1d803eba4fd3697c31b
iman-aazibou <imanaazibou@hotmail.com>
2021-10-02 14:56:04 +0200

    add/display the new expense + initial expenses

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const initialData = [
+  { id: 1, title: "new desk", amount: 300, date: new Date(2021, 5, 12) },
+  { id: 2, title: "new table", amount: 500, date: new Date(2021, 6, 12) },
+  { id: 4, title: "new chair", amount: 200, date: new Date(2021, 8, 12) },
+];
+
 function App() {
-  const data = [
-    { id: 1, title: "new desk", amount: 300, date: new Date(2021, 5, 12) },
-    { id: 2, title: "new table", amount: 500, date: new Date(2021, 6, 12) },
-    { id: 4, title: "new chair", amount: 200, date: new Date(2021, 8, 12) },
-  ];
+  const [expenses, setExpenses] = useState(initialData);
 
   const addExpenseHandler = (expense) => {
-    console.log("App.js");
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+      
+      
+    });
   };
 
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={data} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
